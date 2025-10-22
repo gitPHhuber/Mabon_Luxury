668f666fcf16a9b86a9dcf72c0737fdda25f8ac8
gitPHhuber <gptusers.user1@gmail.com>
2025-10-22 11:12:34 +0300

    fresh dev

--- src/components/ui/skeletons/CollectionCardSkeleton.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Skeleton } from './Skeleton';
-
-export const CollectionCardSkeleton = () => (
-    <div className="group relative block aspect-square overflow-hidden">
-        <Skeleton className="w-full h-full" />
-    </div>
-);
--- src/components/ui/skeletons/Skeleton.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Skeleton = ({ className = '' }: { className?: string }) => (
-    <div className={`bg-gray-200 rounded animate-pulse ${className}`} />
-);
